36ed8b40a9f98189393d01979fba1bf160f6f39a
gcrumpton <gcrumpton@spartaglobal.com>
2025-05-01 14:13:13 +0100

    Add 'PomTest.cs'

---
+++ PomTest.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TestingToolshopDemoWithSelenium.Pages;
+using WebDriverManager.DriverConfigs.Impl;
+using WebDriverManager;
+
+namespace TestingToolshopDemoWithSelenium
+{
+    public class PomTest
+    {
+        private static IWebDriver _driver;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            _driver.Dispose();
+        }
+
+        [Test]
+        public void CheckTitle()
+        {
+            var homePage = new HomePage(_driver);
+
+            homePage.GoToHomePage();
+
+            string pageTitle = _driver.Title;
+            Assert.That(pageTitle, Is.EqualTo("Practice Software Testing - Toolshop - v5.0"));
+        }
+    }
+}
