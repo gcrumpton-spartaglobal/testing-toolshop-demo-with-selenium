ab9089343b240c231b1ebf241f1312030e29e692
gcrumpton <gcrumpton@spartaglobal.com>
2025-05-09 14:31:56 +0100

    Create NavigationStepDefinitions.cs

---
+++ Steps/Navigation/NavigationStepDefinitions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingToolshopDemoWithSelenium.Steps.Navigation
+{
+    public class NavigationStepDefinitions
+    {
+    }
+}
