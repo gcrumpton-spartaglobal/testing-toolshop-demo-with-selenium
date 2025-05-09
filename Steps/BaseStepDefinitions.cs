de80406c073ad6b22c26eb9e858b732b4b656a76
gcrumpton <gcrumpton@spartaglobal.com>
2025-05-09 11:14:56 +0100

    Add fields to BaseStepDefinitions.cs

--- Steps/BaseStepDefinitions.cs
+++ Steps/BaseStepDefinitions.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
 
 namespace TestingToolshopDemoWithSelenium.Steps
 {
     public class BaseStepDefinitions
     {
+        private static IWebDriver _driver;
+        protected Uri GridUrl;
     }
 }
