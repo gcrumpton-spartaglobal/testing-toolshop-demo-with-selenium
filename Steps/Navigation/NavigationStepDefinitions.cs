29514659798b67a76483b1030c986494d14a17ba
gcrumpton <gcrumpton@spartaglobal.com>
2025-05-09 14:34:20 +0100

    Add binding to NavigationStepDefinitions.cs

--- Steps/Navigation/NavigationStepDefinitions.cs
+++ Steps/Navigation/NavigationStepDefinitions.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Reqnroll;
 
 namespace TestingToolshopDemoWithSelenium.Steps.Navigation
 {
+    [Binding]
     public class NavigationStepDefinitions : BaseStepDefinitions
     {
+
     }
 }
