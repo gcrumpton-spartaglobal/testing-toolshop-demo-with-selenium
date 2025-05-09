f47712830c43e57089ce9ebeef7ecfa2955bd823
gcrumpton <gcrumpton@spartaglobal.com>
2025-05-09 11:17:18 +0100

    Add SetUp() and TearDown() methods

--- Steps/BaseStepDefinitions.cs
+++ Steps/BaseStepDefinitions.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
 
 namespace TestingToolshopDemoWithSelenium.Steps
 {
@@ -11,5 +13,23 @@ namespace TestingToolshopDemoWithSelenium.Steps
     {
         private static IWebDriver _driver;
         protected Uri GridUrl;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            GridUrl = new Uri("http://localhost:4444/wd/hub");
+
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("--headless");
+
+            _driver = new RemoteWebDriver(GridUrl, chromeOptions);
+            _driver.Manage().Window.Maximize();
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            _driver.Dispose();
+        }
     }
 }
