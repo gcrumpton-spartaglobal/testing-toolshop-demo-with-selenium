de618e5cceaae28e2e39809fc9fe9904325eb394
gcrumpton <gcrumpton@spartaglobal.com>
2025-05-12 16:18:47 +0100

    Make CheckoutPage.cs inherit WebPage.cs

--- Pages/CheckoutPage.cs
+++ Pages/CheckoutPage.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
 
 namespace TestingToolshopDemoWithSelenium.Pages
 {
-    public class CheckoutPage
+    public class CheckoutPage(IWebDriver driver) : WebPage(driver)
     {
     }
 }
