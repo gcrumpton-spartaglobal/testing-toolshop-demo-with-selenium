a4ed4a27a21ce5b7e71831b49ee92aec576bcf14
gcrumpton <gcrumpton@spartaglobal.com>
2025-05-12 16:27:42 +0100

    Add CheckoutTotal property

--- Pages/CheckoutPage.cs
+++ Pages/CheckoutPage.cs
@@ -4,6 +4,8 @@ namespace TestingToolshopDemoWithSelenium.Pages
 {
     public class CheckoutPage(IWebDriver driver) : WebPage(driver)
     {
+        public IWebElement CheckoutTotal => driver.FindElement(By.XPath("//td[contains(text(),'$')]"));
+
         public void GoToCheckoutPage()
         {
             driver.Navigate().GoToUrl(BaseUrl + "checkout");
