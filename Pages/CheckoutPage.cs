6f4b128d97bdf8360eb8ce26c75d72b0a918c489
gcrumpton <gcrumpton@spartaglobal.com>
2025-05-12 16:49:33 +0100

    Add ItemNames() method

--- Pages/CheckoutPage.cs
+++ Pages/CheckoutPage.cs
@@ -10,5 +10,19 @@ namespace TestingToolshopDemoWithSelenium.Pages
         {
             driver.Navigate().GoToUrl(BaseUrl + "checkout");
         }
+
+        public List<string> ItemNames()
+        {
+            var items = driver.FindElements(By.XPath("//table[contains(@class, 'table-hover')]//td[@class='product-title']"));
+            List<string> itemNames = new List<string>();
+
+            foreach (var item in items)
+            {
+                //var titleElement = item.FindElement(By.ClassName("product-title"));
+                itemNames.Add(item.Text);
+            }
+
+            return itemNames;
+        }
     }
 }
