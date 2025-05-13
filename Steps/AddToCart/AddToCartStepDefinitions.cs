2a9e0a6a46b8cd86c34aad5d6408d8463bc9b8a9
gcrumpton <gcrumpton@spartaglobal.com>
2025-05-13 12:15:21 +0100

    Update AddToCartStepDefinitions.cs
    - AddToCartStepDefinitions.cs now inherits BaseStepDefinitions.cs
    - Logic has been added to WhenIClickTheAddToCartButton() method

--- Steps/AddToCart/AddToCartStepDefinitions.cs
+++ Steps/AddToCart/AddToCartStepDefinitions.cs
@@ -1,15 +1,26 @@
 using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using Reqnroll;
 
 namespace TestingToolshopDemoWithSelenium.Steps.AddToCart
 {
     [Binding]
-    public class AddToCartStepDefinitions
+    public class AddToCartStepDefinitions : BaseStepDefinitions
     {
         [When("I click the add to cart button")]
         public void WhenIClickTheAddToCartButton()
         {
-            throw new PendingStepException();
+            //Wait for the page to load
+            Thread.Sleep(5000);
+            WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(60));
+            wait.Until(d => d.FindElement(By.CssSelector("#btn-add-to-cart")).Displayed);
+
+            // Find the add to cart button
+            var addToCartButton = Driver.FindElement(By.CssSelector("#btn-add-to-cart"));
+
+            // Click the add to cart button
+            addToCartButton.Click();
         }
 
         [Then("I receive a pop-up saying {string}")]
