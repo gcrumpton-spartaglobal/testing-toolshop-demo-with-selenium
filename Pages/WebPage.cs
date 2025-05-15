02831bdac7e19af60e26029cde09e934cd06ef8b
gcrumpton <gcrumpton@spartaglobal.com>
2025-05-15 14:14:06 +0100

    Add CartIcon property to WebPage.cs

--- Pages/WebPage.cs
+++ Pages/WebPage.cs
@@ -7,6 +7,7 @@ namespace TestingToolshopDemoWithSelenium.Pages
         protected const string BaseUrl = "https://practicesoftwaretesting.com/";
 
         public IWebElement SearchBox => driver.FindElement(By.Id("search-query"));
+        public IWebElement CartIcon => driver.FindElement(By.XPath("//a[@class='nav-link' and @data-test='nav-cart']"));
         public string PageTitle => driver.Title;
     }
 }
