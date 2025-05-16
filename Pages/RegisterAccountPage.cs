8877d5a315a5f2405655f401a0c359491bd25bf6
gcrumpton <gcrumpton@spartaglobal.com>
2025-05-16 15:10:01 +0100

    Add FirstNameInput property

--- Pages/RegisterAccountPage.cs
+++ Pages/RegisterAccountPage.cs
@@ -1,6 +1,9 @@
-﻿namespace TestingToolshopDemoWithSelenium.Pages
+﻿using OpenQA.Selenium;
+
+namespace TestingToolshopDemoWithSelenium.Pages
 {
     public class RegisterAccountPage(IWebDriver driver) : WebPage(driver)
     {
+        public IWebElement FirstNameInput => driver.FindElement(By.Id("first_name"));
     }
 }
