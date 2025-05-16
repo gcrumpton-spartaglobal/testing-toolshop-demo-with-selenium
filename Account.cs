e1b736e170e96d60bb723f53a57c5e3886828d5b
gcrumpton <gcrumpton@spartaglobal.com>
2025-05-16 15:23:25 +0100

    Add all of the properties needed for the scenario outline data table

--- Account.cs
+++ Account.cs
@@ -2,5 +2,16 @@
 {
     public class Account
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
