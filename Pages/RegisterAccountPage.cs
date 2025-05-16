c668ba602b99e4c6a7ecf0988ed90d42ab1dfe14
gcrumpton <gcrumpton@spartaglobal.com>
2025-05-16 15:10:20 +0100

    Add LastNameInput property

--- Pages/RegisterAccountPage.cs
+++ Pages/RegisterAccountPage.cs
@@ -5,5 +5,6 @@ namespace TestingToolshopDemoWithSelenium.Pages
     public class RegisterAccountPage(IWebDriver driver) : WebPage(driver)
     {
         public IWebElement FirstNameInput => driver.FindElement(By.Id("first_name"));
+        public IWebElement LastNameInput => driver.FindElement(By.Id("last_name"));
     }
 }
