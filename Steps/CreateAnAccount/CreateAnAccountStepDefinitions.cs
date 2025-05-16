d5e01b154b145c8afcd28677bb1a63efbb006448
gcrumpton <gcrumpton@spartaglobal.com>
2025-05-16 15:45:22 +0100

    Add form filling logic to when step def

--- Steps/CreateAnAccount/CreateAnAccountStepDefinitions.cs
+++ Steps/CreateAnAccount/CreateAnAccountStepDefinitions.cs
@@ -1,5 +1,6 @@
 using System;
 using Reqnroll;
+using TestingToolshopDemoWithSelenium.Pages;
 
 namespace TestingToolshopDemoWithSelenium.Steps.CreateAnAccount
 {
@@ -16,6 +17,23 @@ namespace TestingToolshopDemoWithSelenium.Steps.CreateAnAccount
         public void WhenIFillOutTheFormWithTheFollowingInformation(DataTable dataTable)
         {
             var accounts = dataTable.CreateSet<Account>();
+            var registerAccountPage = new RegisterAccountPage(Driver);
+
+            foreach (var account in accounts)
+            {
+                // Fill out the form with the account information
+                registerAccountPage.FirstNameInput.SendKeys(account.FirstName);
+                registerAccountPage.LastNameInput.SendKeys(account.LastName);
+                registerAccountPage.DateOfBirthInput.SendKeys(account.DateOfBirth);
+                registerAccountPage.StreetInput.SendKeys(account.Street);
+                registerAccountPage.PostalCodeInput.SendKeys(account.PostalCode);
+                registerAccountPage.CityInput.SendKeys(account.City);
+                registerAccountPage.StateInput.SendKeys(account.State);
+                registerAccountPage.CountryInput.SendKeys(account.Country);
+                registerAccountPage.PhoneInput.SendKeys(account.Phone);
+                registerAccountPage.EmailInput.SendKeys(account.Email);
+                registerAccountPage.PasswordInput.SendKeys(account.Password);
+            }
         }
 
         [When("I click on the {string} button")]
