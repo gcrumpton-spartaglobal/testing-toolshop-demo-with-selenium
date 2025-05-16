9b349ac8b55beccb997670161bd933c44ac9573d
gcrumpton <gcrumpton@spartaglobal.com>
2025-05-16 15:11:39 +0100

    Add DateOfBirthInput property

--- Pages/RegisterAccountPage.cs
+++ Pages/RegisterAccountPage.cs
@@ -6,5 +6,6 @@ namespace TestingToolshopDemoWithSelenium.Pages
     {
         public IWebElement FirstNameInput => driver.FindElement(By.Id("first_name"));
         public IWebElement LastNameInput => driver.FindElement(By.Id("last_name"));
+        public IWebElement DateOfBirthInput => driver.FindElement(By.Id("dob"));
     }
 }
