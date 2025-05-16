8d04ce160b008ac2faf3a6c58b287b713d162685
gcrumpton <gcrumpton@spartaglobal.com>
2025-05-16 13:57:21 +0100

    Add SignInPage property to Page.cs

--- Page.cs
+++ Page.cs
@@ -11,5 +11,6 @@ namespace TestingToolshopDemoWithSelenium
     {
         public static HomePage HomePage { get; set; }
         public static CheckoutPage CheckoutPage { get; set; }
+        public static SignInPage SignInPage { get; set; }
     }
 }
