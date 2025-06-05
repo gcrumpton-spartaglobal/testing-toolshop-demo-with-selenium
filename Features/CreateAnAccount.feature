80ea3d7e61b70eae736f288877375f1782c4d22a
gcrumpton <gcrumpton@spartaglobal.com>
2025-06-05 16:07:14 +0100

    Add sad path scenario outline for submitting an empty form

--- Features/CreateAnAccount.feature
+++ Features/CreateAnAccount.feature
@@ -18,3 +18,17 @@ Scenario Outline: I fill out the form with valid information and an account is c
 	| firstName | lastName | dateOfBirth | street        | postalCode | city      | state | country      | phone       | email                  | password         |
 	| John      | Smith    | 01/01/1982  | 1 Fake Street | FA65 9AL   | Fake City | Fake  | United Kingdom | 01234567890 | fake.email@hotmail.com | 52x+8=Hc79FjRTQ9 |
 	| Sarah-Jane | Smith   | 14/04/1987  | 5 Fake Lane | FA89 9YZ   | Fake City | Fake  | United Kingdom | 01433567790 | sarah.j.smith@hotmail.com | 5W4WHsFL!+b$9!pQ |
+
+@sad
+Scenario Outline: I click the "Register" button without filling out the form
+	Given I am on the home page
+	When I click "Sign In" in the nav bar
+	And I click "Register your account"
+	And I fill out the form with the following information:
+		| FirstName   | LastName   | DateOfBirth   | Street   | PostalCode   | City   | State   | Country   | Phone   | Email   | Password   |
+		| <firstName> | <lastName> | <dateOfBirth> | <street> | <postalCode> | <city> | <state> | <country> | <phone> | <email> | <password> |
+	And I click on the "Register" button
+	Then I receive an error message for each field
+	Examples:
+	| firstName | lastName | dateOfBirth | street | postalCode | city | state | country | phone | email | password |
+	|           |          |             |        |            |      |       |         |       |       |          |
--- Features/CreateAnAccount.feature.cs
+++ Features/CreateAnAccount.feature.cs
@@ -166,6 +166,88 @@ this.ScenarioInitialize(scenarioInfo);
             }
             await this.ScenarioCleanupAsync();
         }
+        
+        [NUnit.Framework.TestAttribute()]
+        [NUnit.Framework.DescriptionAttribute("I click the \"Register\" button without filling out the form")]
+        [NUnit.Framework.CategoryAttribute("sad")]
+        [NUnit.Framework.TestCaseAttribute("", "", "", "", "", "", "", "", "", "", "", null)]
+        public async System.Threading.Tasks.Task IClickTheRegisterButtonWithoutFillingOutTheForm(string firstName, string lastName, string dateOfBirth, string street, string postalCode, string city, string state, string country, string phone, string email, string password, string[] exampleTags)
+        {
+            string[] @__tags = new string[] {
+                    "sad"};
+            if ((exampleTags != null))
+            {
+                @__tags = System.Linq.Enumerable.ToArray(System.Linq.Enumerable.Concat(@__tags, exampleTags));
+            }
+            string[] tagsOfScenario = @__tags;
+            System.Collections.Specialized.OrderedDictionary argumentsOfScenario = new System.Collections.Specialized.OrderedDictionary();
+            argumentsOfScenario.Add("firstName", firstName);
+            argumentsOfScenario.Add("lastName", lastName);
+            argumentsOfScenario.Add("dateOfBirth", dateOfBirth);
+            argumentsOfScenario.Add("street", street);
+            argumentsOfScenario.Add("postalCode", postalCode);
+            argumentsOfScenario.Add("city", city);
+            argumentsOfScenario.Add("state", state);
+            argumentsOfScenario.Add("country", country);
+            argumentsOfScenario.Add("phone", phone);
+            argumentsOfScenario.Add("email", email);
+            argumentsOfScenario.Add("password", password);
+            global::Reqnroll.ScenarioInfo scenarioInfo = new global::Reqnroll.ScenarioInfo("I click the \"Register\" button without filling out the form", null, tagsOfScenario, argumentsOfScenario, featureTags);
+#line 23
+this.ScenarioInitialize(scenarioInfo);
+#line hidden
+            if ((global::Reqnroll.TagHelper.ContainsIgnoreTag(scenarioInfo.CombinedTags) || global::Reqnroll.TagHelper.ContainsIgnoreTag(featureTags)))
+            {
+                testRunner.SkipScenario();
+            }
+            else
+            {
+                await this.ScenarioStartAsync();
+#line 24
+ await testRunner.GivenAsync("I am on the home page", ((string)(null)), ((global::Reqnroll.Table)(null)), "Given ");
+#line hidden
+#line 25
+ await testRunner.WhenAsync("I click \"Sign In\" in the nav bar", ((string)(null)), ((global::Reqnroll.Table)(null)), "When ");
+#line hidden
+#line 26
+ await testRunner.AndAsync("I click \"Register your account\"", ((string)(null)), ((global::Reqnroll.Table)(null)), "And ");
+#line hidden
+                global::Reqnroll.Table table2 = new global::Reqnroll.Table(new string[] {
+                            "FirstName",
+                            "LastName",
+                            "DateOfBirth",
+                            "Street",
+                            "PostalCode",
+                            "City",
+                            "State",
+                            "Country",
+                            "Phone",
+                            "Email",
+                            "Password"});
+                table2.AddRow(new string[] {
+                            string.Format("{0}", firstName),
+                            string.Format("{0}", lastName),
+                            string.Format("{0}", dateOfBirth),
+                            string.Format("{0}", street),
+                            string.Format("{0}", postalCode),
+                            string.Format("{0}", city),
+                            string.Format("{0}", state),
+                            string.Format("{0}", country),
+                            string.Format("{0}", phone),
+                            string.Format("{0}", email),
+                            string.Format("{0}", password)});
+#line 27
+ await testRunner.AndAsync("I fill out the form with the following information:", ((string)(null)), table2, "And ");
+#line hidden
+#line 30
+ await testRunner.AndAsync("I click on the \"Register\" button", ((string)(null)), ((global::Reqnroll.Table)(null)), "And ");
+#line hidden
+#line 31
+ await testRunner.ThenAsync("I receive an error message for each field", ((string)(null)), ((global::Reqnroll.Table)(null)), "Then ");
+#line hidden
+            }
+            await this.ScenarioCleanupAsync();
+        }
     }
 }
 #pragma warning restore
