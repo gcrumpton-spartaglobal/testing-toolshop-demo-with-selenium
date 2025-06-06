c2c6d47e38a4c0d959e86651daa274f3ff1a0978
gcrumpton <gcrumpton@spartaglobal.com>
2025-06-06 15:55:42 +0100

    Add ContactPage.cs

---
+++ Pages/ContactPage.cs
@@ -0,0 +1,12 @@
+﻿using OpenQA.Selenium;
+
+namespace TestingToolshopDemoWithSelenium.Pages
+{
+    public class ContactPage(IWebDriver driver) : WebPage(driver)
+    {
+        public void GoToContactPage()
+        {
+            driver.Navigate().GoToUrl(BaseUrl + "contact");
+        }
+    }
+}
