2a3bba7096f34e8e211b651ad7a06c1c9894d0a6
gcrumpton <gcrumpton@spartaglobal.com>
2025-06-06 15:58:27 +0100

    Add EmailInput

--- Pages/ContactPage.cs
+++ Pages/ContactPage.cs
@@ -7,6 +7,7 @@ namespace TestingToolshopDemoWithSelenium.Pages
         #region InputFields
         public IWebElement FirstNameInput => driver.FindElement(By.Id("first_name"));
         public IWebElement LastNameInput => driver.FindElement(By.Id("last_name"));
+        public IWebElement EmailInput => driver.FindElement(By.Id("email"));
         #endregion
 
         public void GoToContactPage()
