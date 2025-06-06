7d7719c163d242e7bcc38ccc7ecbcfa509f64ff4
gcrumpton <gcrumpton@spartaglobal.com>
2025-06-06 17:04:26 +0100

    Add properties to ContactForm.cs

--- ContactForm.cs
+++ ContactForm.cs
@@ -2,5 +2,10 @@
 {
     public record ContactForm
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
     }
 }
