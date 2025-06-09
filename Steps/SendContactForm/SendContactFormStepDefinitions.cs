08846f74920f7bb8a090ac5d31e3514c3503ca44
gcrumpton <gcrumpton@spartaglobal.com>
2025-06-09 10:13:15 +0100

    Add logic to the 'When' step def for filling out the contact form

--- Steps/SendContactForm/SendContactFormStepDefinitions.cs
+++ Steps/SendContactForm/SendContactFormStepDefinitions.cs
@@ -1,15 +1,26 @@
 using System;
 using Reqnroll;
+using TestingToolshopDemoWithSelenium.Pages;
 
 namespace TestingToolshopDemoWithSelenium.Steps.SendContactForm
 {
     [Binding]
-    public class SendContactFormStepDefinitions
+    public class SendContactFormStepDefinitions : BaseStepDefinitions
     {
+        private ContactForm contactForm;
+
         [When("I fill out the contact form with the following information:")]
         public void WhenIFillOutTheContactFormWithTheFollowingInformation(DataTable dataTable)
         {
-            throw new PendingStepException();
+            contactForm = dataTable.CreateInstance<ContactForm>();
+            var contactPage = new ContactPage(Driver);
+
+            // Fill out the contact form
+            contactPage.FirstNameInput.SendKeys(contactForm.FirstName);
+            contactPage.LastNameInput.SendKeys(contactForm.LastName);
+            contactPage.EmailInput.SendKeys(contactForm.Email);
+            contactPage.SubjectInput.SendKeys(contactForm.Subject);
+            contactPage.MessageInput.SendKeys(contactForm.Message);
         }
 
         [Then("I receive a confirmation message")]
