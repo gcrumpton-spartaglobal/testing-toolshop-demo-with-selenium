02d132d6fe244884e58dbb94c62db6e54c4fdde1
gcrumpton <gcrumpton@spartaglobal.com>
2025-06-10 10:36:17 +0100

    Add multiple browser support to step defs

--- Steps/BaseStepDefinitions.cs
+++ Steps/BaseStepDefinitions.cs
@@ -1,37 +1,54 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Remote;
+using TestingToolshopDemoWithSelenium.Pages;
 
 namespace TestingToolshopDemoWithSelenium.Steps
 {
     public class BaseStepDefinitions
     {
-        private static IWebDriver _driver;
         protected Uri GridUrl;
+        public static IWebDriver Driver { get; set; }
+        private DriverOptions _driverOptions;
 
-        public static IWebDriver Driver => _driver;
+        // Allow browser selection per scenario
+        public string Browser { get; set; } = "chrome";
 
         [OneTimeSetUp]
         public void SetUp()
         {
             GridUrl = new Uri("http://localhost:4444/wd/hub");
 
-            var chromeOptions = new ChromeOptions();
-            //chromeOptions.AddArgument("--headless");
+            switch(Browser.ToLower())
+            {
+                case "chrome":
+                    _driverOptions = new ChromeOptions();
+                    break;
+                case "firefox":
+                    _driverOptions = new FirefoxOptions();
+                    break;
+                case "edge":
+                    _driverOptions = new EdgeOptions();
+                    break;
+                default:
+                    throw new ArgumentException($"Unsupported browser: {Browser}");
+            }
 
-            _driver = new RemoteWebDriver(GridUrl, chromeOptions);
-            _driver.Manage().Window.Maximize();
+            Driver = new RemoteWebDriver(GridUrl, _driverOptions.ToCapabilities());
+            Driver.Manage().Window.Maximize();
         }
 
         [OneTimeTearDown]
         public void TearDown()
         {
-            _driver.Dispose();
+            Driver.Dispose();
         }
     }
 }
