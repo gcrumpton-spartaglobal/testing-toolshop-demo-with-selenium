254f159ffa926fb9c5c1374c43cdb5b8391045ac
gcrumpton <gcrumpton@spartaglobal.com>
2025-06-11 15:24:40 +0100

    Add logic to 'When' step def

--- Steps/SignIn/SignInStepDefinitions.cs
+++ Steps/SignIn/SignInStepDefinitions.cs
@@ -1,14 +1,21 @@
 using Reqnroll;
+using TestingToolshopDemoWithSelenium.Pages;
 
 namespace TestingToolshopDemoWithSelenium.Steps.SignIn
 {
     [Binding]
     public class SignInStepDefinitions : BaseStepDefinitions
     {
+        private SignInForm signInForm;
+
         [When("I fill out the sign in form with the following information:")]
         public void WhenIFillOutTheSignInFormWithTheFollowingInformation(DataTable dataTable)
         {
-            throw new PendingStepException();
+            signInForm = dataTable.CreateInstance<SignInForm>();
+
+            // Fill out the form with the account information
+            Page.SignInPage.EmailInput.SendKeys(signInForm.Email);
+            Page.SignInPage.PasswordInput.SendKeys(signInForm.Password);
         }
 
         [Then("I am taken to the admin dashboard")]
