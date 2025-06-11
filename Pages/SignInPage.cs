401e380bf004de0a68e4faf007313562be401adb
gcrumpton <gcrumpton@spartaglobal.com>
2025-06-11 15:13:19 +0100

    Add InputFields region

--- Pages/SignInPage.cs
+++ Pages/SignInPage.cs
@@ -4,12 +4,14 @@ namespace TestingToolshopDemoWithSelenium.Pages
 {
     public class SignInPage(IWebDriver driver) : WebPage(driver)
     {
+        #region InputFields
         public IWebElement LoginText => driver.FindElement(By.XPath("//h3[contains(text(),'Login')]"));
         public IWebElement RegisterAccountLink => driver.FindElement(By.XPath("//a[@data-test='register-link']"));
         public IWebElement ForgotPasswordLink => driver.FindElement(By.XPath("//a[@data-test='forgot-password-link']"));
         public IWebElement EmailInput => driver.FindElement(By.Id("email"));
         public IWebElement PasswordInput => driver.FindElement(By.Id("password"));
         public IWebElement GoogleSignInButton => driver.FindElement(By.ClassName("google-sign-in-button"));
+        #endregion
 
         public void GoToSignInPage()
         {
